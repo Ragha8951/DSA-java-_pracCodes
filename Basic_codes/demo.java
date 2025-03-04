69f5e591a862a2fc2b2678eac769606e505c600f
Raghavendra S <139067634+Ragha8951@users.noreply.github.com>
2025-03-04 22:42:50 +0530

    Add files via upload

---
+++ Basic_codes/demo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class demo {
+    static void printPattern(int rows, int cols) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+               if(i==1||i==rows||j==1||j==cols ){
+                System.out.print("#");
+               }
+               else{
+                System.out.print(" ");
+               }
+            
+              }  System.out.println();
+    }}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+        printPattern(rows, cols);
+        sc.close();
+    }
+}
